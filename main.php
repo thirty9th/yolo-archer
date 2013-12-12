17c60c5efd47f9a10a3a35573c7bbbe405c9570e
Dillon Fisher <thirty9th@gmail.com>
2013-12-12 02:36:17 -0600

    Added Daily Deals to the front page

--- main.php
+++ main.php
@@ -2,6 +2,26 @@
 
 ini_set('session.use_cookies', '1');
 
+// Functions
+function getDeal($connection, $partName) {
+
+	$sql = "select name, description, price, image from part where name=\"" . $partName . "\"";
+	$query = mysqli_query($connection, $sql);
+	if(($row = mysqli_fetch_array($query)) == NULL) {
+		echo "Whoops! looks like you lost your way. Return to the part search page to try again. <br/>";
+		echo "<a href = \"partSearch.php\">Part Search</a>";
+		
+	}
+	
+	$result = "";
+	$result .= '<p class="small-heading">' . $row["name"] . '</p>';
+	$result .= '<div id="rule"></div>';
+	$result .= '<img src="' . $row["image"] . '" alt="Part" /><br /><br />';
+	$result .= '<p>$' . $row["price"] . '</p>';
+	$result .= '<a href="part.php?part=' . $row["name"] . '">Buy Now</a>';
+	echo $result;
+}
+
 // Header material
 echo '
 <html>
@@ -15,6 +35,27 @@ echo '
 <body>';
 
 	include_once('navbar.php');
+	
+	// Database connection
+	$dbHost = "68.191.214.214";
+	$dbUsername = "galefisher";
+	$dbPassword = "galefisher";       
+	$dbTable = "galefisherautoparts";
+	$dbPort = 3306;
+	$con = mysqli_connect($dbHost, $dbUsername, $dbPassword, $dbTable, $dbPort);
+	if (!$con) {
+		exit('Connect Error (' . mysqli_connect_errno() . ') ' . mysqli_connect_error());
+	}
+
+	mysqli_set_charset($con, 'utf-8');
+	
+	$deals = array(
+		0 => "Smooth Tires",
+		1 => "Rough Tires",
+		2 => "New Brakes",
+		3 => "Turbo Engine 480");
+	
+	// Main body
 	echo '
 	<div id="logo-wrapper">
 		<div id="logo-image">
@@ -24,14 +65,18 @@ echo '
 	<div id="content-wrapper">
 		<p class="content-title">Daily Deals</p>
 		<div id="four-square-wrapper">
-			<div id="top-left">
-			</div>
-			<div id="top-right">
-			</div>
-			<div id="bottom-left">
-			</div>
-			<div id="bottom-right">
-			</div>
+			<div id="top-left">';
+				getDeal($con, $deals[0]);
+			echo '</div>
+			<div id="top-right">';
+				getDeal($con, $deals[1]);
+			echo '</div>
+			<div id="bottom-left">';
+				getDeal($con, $deals[2]);
+			echo '</div>
+			<div id="bottom-right">';
+				getDeal($con, $deals[3]);
+			echo '</div>
 		</div>
 	</div>';
 	
